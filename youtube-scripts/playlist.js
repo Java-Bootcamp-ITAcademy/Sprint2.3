3e29ea75f64d4b903ef41423018787063ca8b04d
Ketterle <85057735+Ketterle@users.noreply.github.com>
2023-03-28 00:37:57 +0200

    Delete youtube-scripts directory

--- youtube-scripts/playlist.js
+++
@@ -1,2 +0,0 @@
-
-db.createCollection( 'playlist', {validator: {$jsonSchema: {bsonType: 'object',title:'playlist',required: [         'name',          'creationdate',          'status',          'videos'],properties: {name: {bsonType: 'string'},creationdate: {bsonType: 'date'},status: {enum: 'PUBLIC','PRIVATE'},videos: {bsonType: 'array',items: {bsonType: 'objectId'}}}         }      }});  
